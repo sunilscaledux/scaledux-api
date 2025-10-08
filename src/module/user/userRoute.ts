059d786a7cc1c36d7d59f0cdb72b29b19be03171
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-10-08 15:02:16 +0545

    remove unwanted comment

--- src/module/user/userRoute.ts
+++ src/module/user/userRoute.ts
@@ -35,13 +35,10 @@ router.post("/reset-password", generalRateLimiter, resetPassword);
 router.post("/logout", authenticateToken, logout);
 router.get("/auth/me", authenticateToken, getCurrentUser);
 
-// Google OAuth callback
 router.post("/auth/google-callback", generalRateLimiter, googleCallback);
 
-// LinkedIn OAuth callback
 router.post("/auth/linkedin-callback", generalRateLimiter, linkedinCallback);
 
-// Test cookies  - no authentication required
 router.get("/test-cookies", testCookies);
 
 export default router;
