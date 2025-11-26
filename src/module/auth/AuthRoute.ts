2a23346cbb73af2652ebc4f7d22ea1690aba4216
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-11-26 18:52:20 +0545

    fix import issue

--- src/module/auth/AuthRoute.ts
+++ src/module/auth/AuthRoute.ts
@@ -11,8 +11,8 @@ import {
   verifyOtp,
   resendOtpUnified,
 } from "@module/auth/AuthController";
-import { googleCallback } from "./GoogleAuthController";
-import { linkedinCallback } from "./LinkedinAuthController";
+import { googleCallback } from "@module/auth/GoogleAuthController";
+import { linkedinCallback } from "@module/auth/LinkedinAuthController";
 import { generalRateLimiter, otpRateLimiter } from "@middleware/rateLimiter";
 import { authenticateToken, preventAuthenticatedAccess } from "@middleware/auth";
 
